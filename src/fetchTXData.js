a28d8ca16f301d7d1165322e6d968ed89b804a91
Maciej Caderek <maciej.caderek@gmail.com>
2023-02-05 05:11:13 +0100

    Added backup source (blockchair)

--- src/fetchTXData.js
+++ src/fetchTXData.js
@@ -1,7 +1,46 @@
-async function fetchTXData(txId) {
-  const res = await fetch(`https://mempool.space/api/tx/${txId}/hex`);
+async function fetchFromMempool(txId) {
+  try {
+    const res = await fetch(`https://mempool.space/api/tx/${txId}/hex`);
 
-  return res.text();
+    if (!res.ok) {
+      return null;
+    }
+
+    return res.text();
+  } catch (e) {
+    return null;
+  }
+}
+
+async function fetchFromBlockchair(txId) {
+  try {
+    const res = await fetch(
+      `https://api.blockchair.com/bitcoin/raw/transaction/${txId}`
+    );
+
+    if (!res.ok) {
+      return null;
+    }
+
+    const tx = await res.json();
+    return tx.data[txId]["raw_transaction"];
+  } catch (e) {
+    return null;
+  }
+}
+
+const requests = [fetchFromMempool, fetchFromBlockchair];
+
+async function fetchTXData(txId) {
+  for (const fetchTx of requests) {
+    const tx = await fetchTx(txId);
+
+    if (tx !== null) {
+      return tx;
+    }
+  }
+
+  return null;
 }
 
 export default fetchTXData;
