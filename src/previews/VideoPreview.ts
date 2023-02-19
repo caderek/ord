03ef30999bda053febbb539b4836409e00086126
Maciej Caderek <maciej.caderek@gmail.com>
2023-02-19 03:16:55 +0100

    Fixed video preview bug

--- src/previews/VideoPreview.ts
+++ src/previews/VideoPreview.ts
@@ -1,9 +1,9 @@
 import type { FileData } from "./types.js";
 
 async function VideoPreview({ url, mime, ext, size }: FileData) {
-  const audio = new Audio(url);
-  audio.controls = true;
-  return { el: audio, mime, url, ext, size };
+  const video = document.createElement("video");
+  video.controls = true;
+  return { el: video, mime, url, ext, size };
 }
 
 export default VideoPreview;
